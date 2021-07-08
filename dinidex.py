62d5cd9fbf031e2bea37e8b9288a354ef611906c
Vodiniz <vitor.oliveira.vod@gmail.com>
2021-07-08 00:13:24 -0300

    Logo + qt assets

--- dinidex.py
+++ dinidex.py
@@ -1,4 +1,27 @@
 import pypokedex
+import json
+import requests
+
+
+def get_evolution_json(id):
+
+    pokemon_api = requests.get(url='https://pokeapi.co/api/v2/pokemon/{}'.format(id))
+    pokemon_json = json.loads(pokemon_api.text)
+
+    species_api = (pokemon_json['species'])
+    name, evolution_url = species_api['name'], species_api['url']
+    print(evolution_url)
+    evolution_api = requests.get(evolution_url)
+    evolution_json = json.loads(evolution_api.text)
+    evolution_url = evolution_json['evolution_chain']['url']
+    evolution_id = (evolution_url.split('/'))[-2]
+
+    evolution_page = requests.get(evolution_url)
+    evolution_chain = json.loads(evolution_page.text)
+
+    return evolution_chain
+
+
 
 def call_pokemon(input):
     try:
@@ -18,9 +41,22 @@ def print_pokemon(pokemon):
     print('Types:{}'.format(pokemon.types))
     print('Abilities:{}'.format(pokemon.abilities))
     print('Stats:{}'.format(pokemon.base_stats))
-    print('Sprinte{}'.format(pokemon.sprites))
-    
+    print(list(pokemon.get_descriptions().values())[0])
+    print(pokemon.moves)
+
     return
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def main():
